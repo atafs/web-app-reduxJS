f60220723214db943900a26fee854c03253a6826
americotomas <americoviagens@gmail.com>
2016-11-22 21:10:27 +0000

    DEV: Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './Header'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
+    <Header />
     <AddTodo />
     <VisibleTodoList />
     <Footer />
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Welcome to React</h2>
+    </div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
+
+export default Header
